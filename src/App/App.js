714e2f5ff90968ced1b0632dff68c0a567edc1c4
Marcos Oliveira <marcos.roliver@gmailcom>
2020-04-11 01:52:10 -0300

    Criação do Projeto Consumo de API

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../assets/img/logo.svg';
+import './App.css';
+
+import '../assets/fonts/Roboto-Regular.ttf'
+import '../assets/fonts/Roboto-Bold.ttf'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Busque por um Artista</p>
+        <textarea>Teste</textarea>
+      </header>
+    </div>
+  );
+}
+
+export default App;
